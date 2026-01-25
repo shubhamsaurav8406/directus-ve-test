97d8f62004678a31f57a307ff1167f70cc204321
Robert Mazan <Robert_Mazan@epam.com>
2026-01-25 15:32:21 +0100

    Page list added for VE entry point

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
+const { directusClient } = require('../lib/directus-auth');
+const { readItems } = require('@directus/sdk');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Directus VE' });
+router.get('/', async function(req, res, next) {
+  const pages = await directusClient.request(
+    readItems('pages', {
+      fields: ['title', 'status', 'slug'],
+    })
+  );
+  
+  res.render('index', { pages });
 });
 
 module.exports = router;
